ff44025ccba906d609a7eb4d06584913d198fa46
Ali Rizvi <36699492+AliRizvi433@users.noreply.github.com>
2025-06-06 23:57:13 +0530

    Time: 9 ms (51.19%), Space: 42.8 MB (68.9%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,39 +1,45 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) {
-            return false;
-        }
-
+        if (s1.length() > s2.length()) return false;
         int[] countS1 = new int[26];
         int[] countS2 = new int[26];
 
-        for (char c : s1.toCharArray()) {
-            countS1[c - 'a']++;
+        int matches = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
+            countS1[s1.charAt(i) - 'a']++;
+            countS2[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (countS1[i] == countS2[i]) {
+                matches++;
+            }
         }
 
         int l = 0;
+        for (int r = s1.length(); r < s2.length(); r++) {
+            if (matches == 26)
+                return true;
 
-        for (int r = 0; r < s2.length(); r++) {
-            countS2[s2.charAt(r) - 'a']++;
+            int index = s2.charAt(r) - 'a';
 
-            if ((r - l + 1) == s1.length()) {
-                if (countCheck(countS1, countS2)) {
-                    return true;
-                } else {
-                    countS2[s2.charAt(l) - 'a']--;
-                    l++;
-                }
-            }
+            countS2[index]++;
+            if (countS1[index] == countS2[index])
+                matches++;
+
+            else if (countS1[index] + 1 == countS2[index])
+                matches--;
+
+            index = s2.charAt(l) - 'a';
+            countS2[index]--;
+            if (countS1[index] == countS2[index])
+                matches++;
+
+            else if (countS1[index] - 1 == countS2[index])
+                matches--;
+            l++;
         }
-        return false;
-    }
-
-    public boolean countCheck(int[] a, int[] b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) {
-                return false;
-            }
-        }
-        return true;
+        return matches == 26;
     }
 }
