54e833ecc7e77d80e739506120f41c2d8001c296
Ali Rizvi <36699492+AliRizvi433@users.noreply.github.com>
2025-06-24 22:16:46 +0530

    Time: 80 ms (12.46%), Space: 59.7 MB (37.45%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,27 +1,17 @@
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int[] output = new int[n - k + 1];
-        Deque<Integer> q = new LinkedList<>();
-        int l = 0, r = 0;
-
-        while (r < n) {
-            while (!q.isEmpty() && nums[q.getLast()] < nums[r]) {
-                q.removeLast();
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        int[] output = new int[nums.length - k + 1];
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            heap.offer(new int[]{nums[i], i});
+            if (i >= k - 1) {
+                while (heap.peek()[1] <= i - k) {
+                    heap.poll();
+                }
+                output[idx++] = heap.peek()[0];
             }
-            q.addLast(r);
-
-            if (l > q.getFirst()) {
-                q.removeFirst();
-            }
-
-            if ((r + 1) >= k) {
-                output[l] = nums[q.getFirst()];
-                l++;
-            }
-            r++;
         }
-
         return output;
     }
 }
