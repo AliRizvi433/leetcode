745dc49cc7990653f6e5f15084390852880de2c2
Ali Rizvi <36699492+AliRizvi433@users.noreply.github.com>
2025-09-03 00:36:35 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,24 +1,49 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int[] freq = new int[3];
+        // int[] freq = new int[3];
 
-        for(int x: nums){
-            freq[x]++;
-        }
+        // for(int x: nums){
+        //     freq[x]++;
+        // }
 
-        for(int i = 1; i < 3; i++){
-            freq[i] += freq[i-1]; 
-        }
+        // for(int i = 1; i < 3; i++){
+        //     freq[i] += freq[i-1]; 
+        // }
 
-        for(int i = 0; i < nums.length; i++){
-            if(i < freq[0])
-            nums[i] = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(i < freq[0])
+        //     nums[i] = 0;
             
-            else if(i >= freq[0] && i < freq[1])
-            nums[i] = 1;
+        //     else if(i >= freq[0] && i < freq[1])
+        //     nums[i] = 1;
 
-            else{
-                nums[i] = 2;
+        //     else{
+        //         nums[i] = 2;
+        //     }
+        // }
+
+        int left = 0, mid = 0, right = nums.length - 1;
+        int temp;
+
+
+        while(mid <= right){
+            if(nums[mid] == 1){
+                mid++;
+            }
+
+            else if(nums[mid] == 0){
+                temp = nums[mid];
+                nums[mid] = nums[left];
+                nums[left] = temp;
+                mid++;
+                left++;
+            }
+
+            else if(nums[mid] == 2){
+                temp = nums[mid];
+                nums[mid] = nums[right];
+                nums[right] = temp;
+                right--;
             }
         }
 
